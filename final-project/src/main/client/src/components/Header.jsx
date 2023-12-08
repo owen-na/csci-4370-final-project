7b5f9530f14fc6d53999784c4ec1e5d1334f2b91
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-12-08 08:50:26 -0500

    fix link

--- final-project/src/main/client/src/components/Header.jsx
+++ final-project/src/main/client/src/components/Header.jsx
@@ -1,41 +1,45 @@
 import logo from "../assets/logo.svg";
 import "../styling/Header.css";
-import {useState} from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   async function handleSubmit(event) {
     event.preventDefault(); // prevent default page refreshing
     /*
-    * Sending a post request to the server to with the username and password
-    */
+     * Sending a post request to the server to with the username and password
+     */
     const response = await fetch("http://localhost:8080/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ "username" : username , "password" : password}),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
     });
 
     if (response.ok) {
-        console.log("Received response from server");
+      console.log("Received response from server");
     } else {
-        console.error("Error while sending POST request");
+      console.error("Error while sending POST request");
     }
   }
 
   return (
     <header>
       <div className="wrapper">
-        <div className="logo-wrapper">
-          <button className="logo-button">
-            <img src={logo} alt="temo logo"></img>
-          </button>
-        </div>
-       <Link to="pages/SignUp"><button>Login/Register</button></Link>
+        <Link to="/">
+          <div className="logo-wrapper">
+            <button className="logo-button">
+              <img src={logo} alt="temo logo"></img>
+            </button>
+          </div>
+        </Link>
+        <Link to="pages/SignUp">
+          <button>Login/Register</button>
+        </Link>
       </div>
     </header>
   );
