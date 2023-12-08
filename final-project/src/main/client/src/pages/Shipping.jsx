d2559ff901913cb77aa19d71aac295cdb6a04a96
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-12-08 12:12:44 -0500

    shipping

--- final-project/src/main/client/src/pages/Shipping.jsx
+++ final-project/src/main/client/src/pages/Shipping.jsx
@@ -5,27 +5,22 @@ export default function Shipping() {
       <div className>
         <h1>Shipping Information</h1>
       </div>
-      <form action="/shipping">
+      <form action="/shipping" method="post">
         <label for="address">Street Address</label>
-        <input type="text" id="street-address"></input>
-
-        <label for="town-city">Town/City</label>
-        <input type="text" id="town-city"></input>
-        <label for="state">State</label>
-        <input type="text" id="state"></input>
-
-        <label for="zipcode">Zipcode</label>
-        <input type="text" id="zipcode"></input>
+        <input type="text" id="address"></input>
         <label for="country">Country</label>
         <input type="text" id="country"></input>
+        <input type="submit" value="Submit"></input>
+      </form>
+      <form action="/payment" method="post">
         <h1> Payment Information</h1>
-        <label for="name">Name</label>
-        <input type="text" id="name"></input>
-        <label for="pzipcode">Zipcode</label>
-        <input type="text" id="pzipcode"></input>
+        <label for="name_on_card">Name</label>
+        <input type="text" id="name_on_card"></input>
+        <label for="zipcode">Zipcode</label>
+        <input type="text" id="zipcode"></input>
 
-        <label for="cardnumber">Card Number</label>
-        <input type="text" id="cardnumber"></input>
+        <label for="card_number">Card Number</label>
+        <input type="text" id="card_number"></input>
 
         <input type="submit" value="Submit"></input>
       </form>
